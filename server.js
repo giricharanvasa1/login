5fe575177d75d4105ec84c34a6603e55d63e335e
giricharanvasa1 <giricharanvasa1@gmail.com>
2026-04-21 22:31:28 +0530

    server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 app.use(cors());
 
-// Dummy users (for testing)
+
 const users = [
   { username: "admin", password: "1234" },
   { username: "charan", password: "5678" }
@@ -17,12 +17,12 @@ const users = [
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  // Print user input (for learning)
+  
   console.log("User entered:");
   console.log("Username:", username);
   console.log("Password:", password);
 
-  // Check credentials (but don't reveal result)
+
   const user = users.find(
     u => u.username === username && u.password === password
   );
@@ -33,11 +33,12 @@ app.post("/login", (req, res) => {
     console.log("Invalid login ❌");
   }
 
-  // 🔒 Always send same response (no info to frontend)
+ 
   res.json({ status: "ok" });
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
